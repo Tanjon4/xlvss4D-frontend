9b1225196ff3d89288592e73b926ba32ea2fa2ed
Tanjona <andrianiainatanjona67@gmail.com>
2025-07-09 19:43:56 +0300

    activate tailwind css

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+const Home = () => {
+  return (
+    <div className="flex justify-center items-center h-screen bg-green-100">
+      <h1 className="text-3xl font-bold text-green-600">Hello World</h1>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/pages/Produits.jsx
@@ -0,0 +1,9 @@
+const Produits = () => {
+  return (
+    <div>
+      <h1>Produits</h1>
+      <p>Liste des produits disponibles.</p>
+    </div>
+  );
+}
+export default Produits;
