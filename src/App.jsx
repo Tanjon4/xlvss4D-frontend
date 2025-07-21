5aee9ed7af52c11e4b4fab150d123e4bbb99c2e3
Mamy <mamitianarandrianantenaina253@gmail.com>
2025-07-21 09:12:44 +0300

    config langue with zustand

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Produits from "./pages/Produits";
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css"
+import { useLangStore } from "./store/language.store.jsx";
+import { useTranslation } from "react-i18next";
+import LanguageSwitcher from "./components/testLangue.jsx";
+
 
 const App = () => {
+
+  const lang = useLangStore();
+  const i18n = useTranslation();
+  useEffect(() => {
+    i18n.i18n.changeLanguage(lang.lang);
+    document.documentElement.lang = lang.lang;
+  }, [lang]);
   return (
-    <BrowserRouter>
-      <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/produits" element={<Produits />} />
-      </Routes>
-    </BrowserRouter>
+    
+    // <BrowserRouter>
+    //   <Routes>
+    //       <Route path="/" element={<Home />} />
+    //       <Route path="/produits" element={<Produits />} />
+    //   </Routes>
+    // </BrowserRouter>
+    <div>
+      <h1 className="text-blue-700">{i18n.t("welcome")}</h1>
+      {/* ... */}
+      <LanguageSwitcher /> 
+
+    </div>
   );
 }
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import React from 'react'
 import './index.css'
+import './translations/i18next.jsx'
 
 createRoot(document.getElementById('root')).render(<App />)
