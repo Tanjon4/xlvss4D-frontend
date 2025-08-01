8a420d8ad5dfb4e492ff9e503fe782877a6b9b5c
Sarannah <sarannahsarannah@gmail.com>
2025-08-01 15:43:05 +0000

    footer in Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import Header from "../components/layout/Header";
 import Navigation from "../components/layout/Navigation";
+import Footer from "../components/layout/Footer";
 
 const Home = () => {
   return (
     <>
     <Header/>
     <Navigation />
+    <Footer />
+
     
     </>
   );
