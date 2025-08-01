b83819ea720955eeb078d5b5882bd0a5a5f3a54f
Mihaja <andriamihajaratsimbazafy600@gmail.com>
2025-08-01 14:39:10 +0300

    navbar components

--- src/components/layout/Navigation.jsx
+++ src/components/layout/Navigation.jsx
@@ -3,25 +3,35 @@ import { Link } from 'react-router-dom';
 const Navigation = () => {
     return (
         <>
-        <nav className="flex flex-col">
-            <Link to="/" className="text-2xl font-bold text-gray-900 dark:text-white mr-2">
+        <nav className="flex bg-white justify-around text-black items-center p-8 shadow-md">
+            <div>
+                <h1 className='font-bold text-2xl'>Blanca</h1>
+            </div>
+            <div className="">
+            <Link to="/" className="text-base ml-10">
                 Home
             </Link>
-            <Link to="/contact" className="text-2xl font-bold text-gray-900 dark:text-white mr-2">
+            <Link to="/contact" className="text-base ml-10">
                 Contacts
             </Link>
-            <Link to="/produit" className="text-2xl font-bold text-gray-900 dark:text-white mr-2">
+            <Link to="/produit" className="text-base ml-10">
                 Produits
             </Link>
-            <Link to="/login" className="text-2xl font-bold text-gray-900 dark:text-white mr-2">
+            <Link to="/login" className="text-base ml-10">
                 Login
             </Link>
-            <Link to="/register" className="text-2xl font-bold text-gray-900 dark:text-white mr-2">
+            <Link to="/register" className="text-base ml-10">
                 Register
             </Link>
-            <Link to="/lists" className="text-2xl font-bold text-gray-900 dark:text-white mr-2">
+            <Link to="/lists" className="text-base ml-10">
                 products list
             </Link>
+            </div>
+        <div>
+            <Link to="/serch" className="text-base ml-5">
+                Search
+            </Link>
+        </div>   
         </nav>
         </>)
 }
