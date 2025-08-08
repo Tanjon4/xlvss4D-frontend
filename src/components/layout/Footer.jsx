a12bf7e990a6091f98934a619fcbe4820da36bbf
Sarannah <sarannahsarannah@gmail.com>
2025-08-08 15:17:32 +0000

    New Footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,44 +1,36 @@
 import { Link } from "react-router-dom";
-import '@fortawesome/fontawesome-free/css/all.min.css'; 
+
 
 const Footer = () => {
   return (
-    <footer style={{ display: "flex", justifyContent: "space-between", padding: "1rem" }}>
-      <div>
-        <div style={{ display: "flex", gap: "1rem", alignItems: "center" }}>
-            <h5><Link to="">Home</Link></h5>
-            <h5><Link to="">Contact</Link></h5>
-            <h5><Link to="">Login</Link></h5>
+    <footer className="px-8 pt-2">
+      <div className="flex ">
+        <div className="w-[50%]">
+          <ul className="list-none flex space-x-12">
+            <li><Link to="">Home</Link></li>
+            <li><Link to="">Contact</Link></li>
+            <li><Link to="">Login</Link></li>
+          </ul>
+          
         </div>
-
-        <div style={{ marginTop: "2rem" }}>
-            <h5>
-                ©2025 – Your trusted online store by{" "}
-                <span style={{ color: "red" }}>e-commerce</span>
-            </h5>
+        <div className="w-[50%] text-right space-x-8">
+            <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-facebook-f" style={{ color: "#4b4747ff", fontSize: "0.8rem" }}></i>
+            </a>
+            <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-instagram" style={{ color: "#4b4747ff", fontSize: "0.8rem" }}></i>
+            </a>
+            <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-x-twitter" style={{ color: "#4b4747ff", fontSize: "0.8rem" }}></i>
+            </a>
         </div>
       </div>
-
-      <div
-        style={{
-            display: "flex",
-            gap: "1rem",
-            alignItems: "center",
-            justifyContent: "flex-start", // icônes alignées à gauche
-            width: "150px" // ajuste selon l'espace souhaité
-        }}
-        >
-        <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
-            <i className="fab fa-facebook-f" style={{ color: "#1877F2", fontSize: "1.5rem" }}></i>
-        </a>
-        <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
-            <i className="fab fa-instagram" style={{ color: "#E4405F", fontSize: "1.5rem" }}></i>
-        </a>
-        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-            <i className="fab fa-x-twitter" style={{ color: "#000", fontSize: "1.5rem" }}></i>
-        </a>
+      <div style={{ marginTop: "2rem" }}>
+          <p>
+            ©2025 – created by Mamy, Sarannah, Mihaja, Tanjona, Dayan 
+          </p>
+          <p>e-commerce</p>
         </div>
-
     </footer>
   );
 };
