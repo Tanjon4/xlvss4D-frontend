487e6b29773ce0be47e97616f8bf53a4d200efbc
Mihaja <andriamihajaratsimbazafy600@gmail.com>
2025-08-08 13:37:34 +0300

    Lorem

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -6,6 +6,7 @@ const NotFound = () => {
       <Navigation />
       <h1>404</h1>
       <p>Page not found</p>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime facilis minus quas blanditiis dignissimos. Illo, quis nisi sed distinctio consequuntur illum at facere quisquam aperiam porro quia. Tenetur, provident dolore.</p>
     </div>
   );
 }
